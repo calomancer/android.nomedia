f5f1f87114ba99f95956daac41304be24ee715d9
Jacob <contact@jakeisawesome.com>
2023-04-25 21:16:05 -0400

    Version 1 completed

--- main.ts
+++ main.ts
@@ -1,137 +1,59 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+/*
+ * This plugin was developed with the assistance of ChatGPT, a language model created by OpenAI.
+ * https://openai.com/
+ */
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { Plugin } from 'obsidian';
+import * as path from 'path';
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	private updateTimer: number;
 
 	async onload() {
-		await this.loadSettings();
+		console.log('My plugin has loaded!');
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Run the plugin when it is first loaded
+		await this.updateFolders();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// Register an interval to check for updates to the folder structure
+		this.updateTimer = window.setInterval(async () => {
+			await this.updateFolders();
+		}, 5000);
+	}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+	async onunload() {
+		window.clearInterval(this.updateTimer);
+	}
+
+	async updateFolders() {
+		const vault = this.app.vault;
+		const fm = this.app.vault.adapter;
+
+		// Get all the files in the vault
+		const files = vault.getMarkdownFiles();
+
+		// Loop through each file
+		for (const file of files) {
+			// Get the parent directory of the file
+			const dirPath = path.dirname(file.path);
+			if (!dirPath) {
+				console.log(`Unable to find parent folder for file ${file.path}`);
+				continue;
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			const nomediaPath = path.join(dirPath, '.nomedia');
+
+			// Check if the .nomedia file already exists in the directory or its parent
+			if (!await vault.adapter.exists(nomediaPath)) {
+				const parentDirPath = path.dirname(dirPath);
+				const parentNomediaPath = path.join(parentDirPath, '.nomedia');
+				if (!await vault.adapter.exists(parentNomediaPath)) {
+					// If not, create the .nomedia file in the parent directory
+					await vault.create(parentNomediaPath, 'nomedia');
+					await fm.append(path.normalize(parentDirPath), '.nomedia');
+					console.log(`Created ${parentNomediaPath}`);
 				}
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		}
 	}
 }
